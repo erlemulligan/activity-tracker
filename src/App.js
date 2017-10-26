47bc25370bef7543fefb6de871fa7e8776ba8703
Erle Mulligan <erlemulligan@gmail.com>
2017-10-25 22:09:25 -0500

    adding hours/minutes and start/stop time incrementing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TimerComponent />
+        <TimerComponent breakReminderInSeconds={12} />
       </div>
     );
   }
--- src/Components/TimerComponent/index.js
+++ src/Components/TimerComponent/index.js
@@ -4,30 +4,60 @@ import './TimerComponent.css';
 
 class TimerComponent extends Component {
   constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
+    super(props)
+    console.log(this.props)
+    this.state = {
+      date: new Date(),
+      secondsElapsed: 0,
+    }
 
-    this.handleStart = this.handleStart.bind(this);
-    this.handleStop = this.handleStop.bind(this);
+    this.handleStart = this.handleStart.bind(this)
+    this.handleStop = this.handleStop.bind(this)
+    this.getSeconds = this.getSeconds.bind(this)
+    this.getMinutes = this.getMinutes.bind(this)
+    this.getHours = this.getHours.bind(this)
   }
 
   handleStart() {
-    alert('starting...')
+    this.timeIncrementer = setInterval(() => {
+      this.setState({secondsElapsed: this.state.secondsElapsed + 1})
+
+      if (this.state.secondsElapsed === this.props.breakReminderInSeconds) {
+        alert('please take a break')
+      }
+    }, 1000)
   }
 
   handleStop() {
-    alert('stopping...')
+    clearInterval(this.timeIncrementer)
+    this.setState({secondsElapsed: 0})
+  }
+
+  getHours() {
+    return ('0' + Math.floor(this.state.secondsElapsed / 3600)).slice(-2)
+  }
+
+  getMinutes() {
+    return ('0' + Math.floor(this.state.secondsElapsed / 60) % 60).slice(-2)
+  }
+
+  getSeconds() {
+    return ('0' + this.state.secondsElapsed % 60).slice(-2)
   }
 
   render() {
     return (
       <div className="TimerComponentContainer">
-        <h1>0:00</h1>
+        <h1>{`${this.getHours()}:${this.getMinutes()}:${this.getSeconds()}`}</h1>
         <ButtonComponent buttonText="Start" onClick={this.handleStart}/>
         <ButtonComponent buttonText="Stop" onClick={this.handleStop} />
       </div>
     );
   }
+}
+
+TimerComponent.defaultProps = {
+  breakReminderInSeconds: 5,
 }
 
 export default TimerComponent;
